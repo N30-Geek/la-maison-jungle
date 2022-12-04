69bb05272b51278690f31bbce0611d02ebfc22f2
Néo <dryikobu967@protonmail.com>
2022-12-04 21:33:28 +0100

    Partie sur les itération et les conditions ternair

--- src/compoments/PlantesList.js
+++ src/compoments/PlantesList.js
@@ -1,19 +1,39 @@
-const plantList = [
-    'monstera',
-    'ficus lyrata',
-    'pothos argenté',
-    'yucca',
-    'palmier'
-]
+import { plantList } from '../datas/plantList'
+import CareScale from './CareScale'
+import '../styles/ShoppingList.css'
 
-function PlantesList() {
+function ShoppingList() {
+    const categories = plantList.reduce(
+        (acc, plant) =>
+            acc.includes(plant.category) ? acc : acc.concat(plant.category),
+        []
+    )
+
     return (
-        <ul>
-            {plantList.map(plant => (
-                <li>{plant}</li>
-            ))}
-        </ul>
+        <div>
+            <ul>
+                {categories.map((cat) => (
+                    <li key={cat}>{cat}</li>
+                ))}
+            </ul>
+            <ul className='lmj-plant-list'>
+                {plantList.map((plant) => (
+                    plant.isSpecialOffer ?
+
+                        <li key={plant.id} className='lmj-plant-item'>
+                            <div className="lmj-sales">
+                                {plant.name}{plant.isBestSale ? <span>🔥</span> : null}
+                                <CareScale scaleValue={plant.light} />
+                            </div>
+                        </li>
+                        : <li key={plant.id} className='lmj-plant-item'>
+                            {plant.name}{plant.isBestSale ? <span>🔥</span> : null}
+                            <CareScale scaleValue={plant.light} />
+                        </li>
+                ))}
+            </ul>
+        </div>
     )
 }
 
-export default PlantesList;
+export default ShoppingList;
---
+++ src/datas/plantList.js
@@ -0,0 +1,73 @@
+export const plantList = [
+    {
+        name: 'monstera',
+        category: 'classique',
+        id: '1ed',
+        isBestSale: true,
+        light: 1,
+        water: true
+    },
+    {
+        name: 'ficus lyrata',
+        category: 'classique',
+        id: '2ab',
+        isBestSale: true,
+        light: 2,
+        water: true
+
+    },
+    {
+        name: 'pothos argenté',
+        category: 'classique',
+        id: '3sd',
+        isSpecialOffer: true,
+        light: 3,
+        water: true
+    },
+    {
+        name: 'yucca',
+        category: 'classique',
+        id: '4kk',
+        light: 2,
+        water: true
+    },
+    {
+        name: 'olivier',
+        category: 'extérieur',
+        id: '5pl',
+        isBestSale: true,
+        light: 1,
+        water: true
+    },
+    {
+        name: 'géranium',
+        category: 'extérieur',
+        id: '6uo',
+        light: 1,
+        water: true
+    },
+    {
+        name: 'basilique',
+        category: 'extérieur',
+        id: '7ie',
+        isSpecialOffer: true,
+        light: 3,
+        water: true
+    },
+    {
+        name: 'aloe',
+        category: 'plante grasse',
+        id: '8fp',
+        isSpecialOffer: true,
+        light: 2,
+        water: true
+    },
+    {
+        name: 'succulente',
+        category: 'plante grasse',
+        id: '9vn',
+        isBestSale: true,
+        ligh: 1,
+        water: true
+    }
+]
